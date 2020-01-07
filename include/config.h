5622991992f295e99abbac40cd8c18a33ae1a052
Anton Rezanov <ton@specadmin.ru>
2020-01-07 22:41:25 +0300

    Default configuration file updated

--- include/config.h
+++ include/config.h
@@ -4,11 +4,12 @@
 #include <avr/io.h>
 //----------------------------------------------------------------------------
 
-// #define DEBUG_PORT          PORTD
-// #define DEBUG_DDR           DDRD
-// #define DEBUG_BIT           2
-// #define DEBUG_BAUD_RATE     1152000
+#define DEBUG_PORT          PORTD
+#define DEBUG_DDR           DDRD
+#define DEBUG_BIT           1
+#define DEBUG_BAUD_RATE     1152000
 // #define DEBUG_EXTENDED_DUMP
+//#define RAM_GUARD_ENABLED
 
 //----------------------------------------------------------------------------
 #endif
