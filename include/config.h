92e7e37106a5d424ecc4f2e179078f0543c71b53
Anton Rezanov <ton@specadmin.ru>
2020-05-10 12:36:14 +0300

    Default debug pin changed to MISO

--- include/config.h
+++ include/config.h
@@ -4,9 +4,9 @@
 #include <avr/io.h>
 //----------------------------------------------------------------------------
 
-#define DEBUG_PORT          PORTD
-#define DEBUG_DDR           DDRD
-#define DEBUG_BIT           1
+#define DEBUG_PORT          PORTB
+#define DEBUG_DDR           DDRB
+#define DEBUG_BIT           4
 #define DEBUG_BAUD_RATE     1152000
 // #define DEBUG_EXTENDED_DUMP
 //#define RAM_GUARD_ENABLED
