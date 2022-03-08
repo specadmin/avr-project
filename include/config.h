9c630bd667c9707d9c9a49a9f975dadb9896bb3b
Anton Rezanov <ton@specadmin.ru>
2022-03-08 16:46:16 +0300

    Default debug pin changed to MISO

--- include/config.h
+++ include/config.h
@@ -6,7 +6,7 @@
 
 // #define DEBUG_PORT          PORTB
 // #define DEBUG_DDR           DDRB
-// #define DEBUG_BIT           4
+// #define DEBUG_BIT           6
 // #define DEBUG_BAUD_RATE     1152000
 // #define DEBUG_EXTENDED_DUMP
 // #define RAM_GUARD_ENABLED
